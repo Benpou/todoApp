cbd4f82a18de95c41e0b32a10fc42aa2888462b3
ben <benpou2012@gmail.com>
2017-10-09 19:03:04 -0700

    Finish lec-19

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+
 const app = express();
 const port = 3000;
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/todoapp';
+
 
 //body Parser middleware
 app.use(bodyParser.json());
@@ -14,11 +19,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Connect to mongodb
+MongoClient.connect(url, (err, database) => {
+   console.log('MongoDB conntected ....');
+   if (err) throw err;
+
+   db = database;
+   Todos = db.collection('todos');
+
+    app.listen(port, () => {
+        console.log('Server running on port: ' + port);
+    });
+});
+
 app.get('/', (req, res, next) => {
-    res.render('index');
+    Todos.find({}).toArray((err, todos) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(todos);
+       res.render('index', {
+           todos: todos
+       });
+    });
 });
 
 
-app.listen(port, () => {
-    console.log('Server running on port: ' + port);
-});
+
